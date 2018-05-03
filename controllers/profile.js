ceea1293ee7ece753f1fe6ea89527f2e3ae4d5ac
Diego Ballesteros <diballesterosc@gmail.com>
2018-05-03 17:50:47 -0500

    Folder Structure, Knex, DB Connection

---
+++ controllers/profile.js
@@ -0,0 +1,18 @@
+const handleProfileGet = (req, res, db, bcrypt) => {
+    const { id } = req.params;
+
+    db.select('*').from('users').where({
+        id: id
+    }).then(user => {
+        if (user.length) {
+            res.json(user[0])
+        }
+        else {
+            res.status(400).json('Error getting user')
+        }
+    }).catch(err => res.status(400).json('Not Found'))
+}
+
+module.exports = {
+    handleProfileGet: handleProfileGet
+}
--- server.js
+++ server.js
@@ -1,122 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'Aranda123',
+        database: 'smart-brain'
+    }
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 })
 
-app.post('/signin', (req, res) => {
-
-   /*  // Load hash from your password DB
-    bcrypt.compare("bacon", hash, function(err, res) {
-    //res == true
-    });
-    
-    bcrypt.compare("veggies", hash, function(err, res){
-        //res == false
-    });
- */
-    if (req.body.email === database.users[0].email
-        && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    }
-    else {
-        res.status(400).json('error logging in');
-    }
-})
-
-
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-
-  /*   bcrypt.hash(password, null, null, function(err, hash) {
-        //Store hash in your password DB
-        console.log(hash);
-    }); */
-    
-    database.users.push(
-        {
-            id: '125',
-            name: name,
-            email: email,
-            password: password,
-            entries: 0,
-            joined: new Date()
-        }
-    )
-
-    res.json(database.users[database.users.length - 1])
-})
-
-app.get('/profile/:id', (req,res) => {
-    const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            res.json(user);
-        }
-    })
-    if(!found) {
-        res.status(400).json('Not Found');
-    }
-
-})
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            user.entries++;
-            res.json(user.entries);
-        }
-    })
-    if(!found) {
-        res.status(400).json('Not Found');
-    }
-})
-
-
-
-
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db, bcrypt) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(3000, () => {
     console.log('App is running on on port 3000');
 })
-
-/* First Describe what you want your API to do
-/ --> res = this is working
-/signin -- > POST = success/fail
-/register --> POST = user
-/profile/:userid --> GET = user
-/image --> PUT --> user
-*/
